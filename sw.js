ebeeaa3e1b68d170670e60a01aabbc49b2d12b5a
rarekay <45952041+rarekay@users.noreply.github.com>
2026-06-24 17:18:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'lexiclock-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/explore.js',
+  '/manifest.json',
+  '/icons/icon.svg',
+  '/words/csw.txt',
+  '/words/nwl2023.txt',
+  '/words/wotd.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
